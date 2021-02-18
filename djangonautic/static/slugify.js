a720e6d42aca9afbb6e0694193e54635b2b71815
LendupLepcha <lenduplepcha62@gmail.com>
2021-02-18 14:48:14 +0530

    add JS

--- djangonautic/static/slugify.js
+++ djangonautic/static/slugify.js
@@ -1,13 +1,12 @@
-const titleInput = document.querySelector('input[name=title]');
-const slugInput = document.querySelector('slug[name=slug]');
-// const titleInput = document.getElementById("input[name=id_title]");
-// const slugInput = document.getElementById("input[name=id_slug]");
+// const titleInput = document.querySelector('name=title');
+// const slugInput = document.querySelector('name=slug');
+const titleInput = document.getElementById("id_title");
+const slugInput = document.getElementById("id_slug");
 const slugify = (val) => {
     return val.toString().toLowerCase().trim()
         .replace(/&/g, '-and-')    // replace & with -and-
         .replace(/[\s\W-]+/g, '-') // replace space and non word char and dash with -
 };
 titleInput.addEventListener('keyup', (e)=>{
-    // slugInput.setAttribute('value', slugify(titleInput.value));
     slugInput.setAttribute('value', slugify(titleInput.value));
 });
